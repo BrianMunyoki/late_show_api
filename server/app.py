9167f111101f04548864a7f473fcce36019c625d
Brian Munyoki <brianmunyoki168@gmail.com>
2025-11-23 23:16:24 +0300

    configuring app,py

--- server/app.py
+++ server/app.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_restful import Api
 
+#create Flask app
 app=Flask(__name__)
 
+#configureing SQLITE database
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+#initialize extensions
+db=SQLAlchemy(app)
+migrate=Migrate(app,db)
+api=Api(app)
 @app.route('/')
 
 def index():
