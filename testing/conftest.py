6a4f6ad873c759d09fdc8cd99ab1b134b8a58ff9
Brian Muema <brianmunyoki168@gmail.com>
2025-12-08 19:51:44 +0300

    changed path on conftest.py, installd pip install werkzeug==2.3.7 , changed files to the root directory.

--- testing/conftest.py
+++ testing/conftest.py
@@ -1,3 +1,7 @@
+import sys, os
+ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, ROOT_PATH)
+
 import pytest
 from app import app, db
 from models import Episode, Guest, Appearance
